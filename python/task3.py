300a0cb1717f31d0e052bd1ac6a4a9c460e3e7a0
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-03-19 14:47:44 +0200

    complete task3

--- python/task3.py
+++ python/task3.py
@@ -1,26 +1,32 @@
 baseinput = int(input())
 userinput = int(input())
 
-def NextBase(Base, Number):
-    if Base == 0:
-        return "0"
-    
-    digits = []  
-
-    while Number != 0:
-        x = Number % Base  # Fixed: Calculate modulo with Base, not Base % Base
-        if x < 0:
-            x += Base
-        digits.append(str(x))
-        Number //= Base  # Fixed: Divide by Base, not Base //= Base
-    return "".join(reversed(digits))
-
 Array = []
+
+def remainder(base,Number):
+    wholeNumber = Number/base
+    remainder = Number%base
+    return (remainder,int(wholeNumber))
+
+def Decrypt(base,Number):
+    Array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+         'U', 'V', 'W', 'X', 'Y', 'Z']
+    
+    output = str()
+    number = Number
+    while number != 0:
+        output += Array[int(remainder(base,number)[0])]
+        number = int(remainder(base,number)[1])
+    return output
+
 
 
 while(userinput != -1):
 
-    Array.append(NextBase(baseinput, userinput))
+    reversedoutput = Decrypt(baseinput,userinput)[::-1]
+    Array.append(reversedoutput)
     userinput = int(input())
 
 for i in Array:
