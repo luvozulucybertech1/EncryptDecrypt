e4d4e0f0ddc2954d5c65a01cef13ad1d36137fb7
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-03-20 19:10:07 +0200

    complete task1

--- c++/task1.cpp
+++ c++/task1.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main(){
 
-    cout<<"Encrypt\n";
+    string input = "";
+
+    getline(cin,input);
+    
+
+    int Array[50];
+    for(int index = 0 ; index < input.size();index++){
+        Array[index] = input[index];
+    }
+
+    for(int index = 0 ; index < input.size();index++){
+        cout<<int(Array[index])<<" ";
+    }
+    cout<<endl;
 
     return 0;
 }
