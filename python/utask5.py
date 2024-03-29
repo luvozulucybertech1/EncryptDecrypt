c37f64c71eec43e7f7f507ab93d949904fde915c
luvozulucybertech1 <160118100+luvozulucybertech1@users.noreply.github.com>
2024-03-29 04:43:25 +0200

    Update utask5.py
    decrypt next

--- python/utask5.py
+++ python/utask5.py
@@ -1,12 +1,42 @@
 from string import digits,ascii_uppercase,whitespace
 
-userinput = str()
-
 userinput = str(input())
-
-Array  = [len(userinput)]
 
 Array = [ord(c) for c in userinput]
 
+
+StringsArray = []
+
+def index(character):
+    Array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+         'U', 'V', 'W', 'X', 'Y', 'Z']
+    index = -1
+    for i in range(0,len(Array)):
+        if character == Array[i]:
+            index = i
+            break
+    return index
+
+def Encrypt(base,Number):
+    output = 0
+    for i in Number:
+        output += index(i) * pow(base, len(Number) - 1 - index(i))
+    return output
+
+def tobase(decimal_num, base):
+    result = ""
+
+    while decimal_num > 0:
+        remainder = decimal_num % base
+        if remainder < 10:
+            result = str(remainder) + result
+        else:
+            result = chr(ord('A') + remainder - 10) + result
+        decimal_num //= base
+
+    return result
+
 for i in Array:
-    print(i, end=" ")
+    print(tobase(i,16))
