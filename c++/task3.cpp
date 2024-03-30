a740b8eae06ff42f0172dfbcc93e261ff5fdd40e
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-03-30 16:54:28 +0200

    correct task3

--- c++/task3.cpp
+++ c++/task3.cpp
@@ -1,60 +1,54 @@
 #include <iostream>
-#include <windows.h>
 #include <algorithm>
 #include <utility>
+#include <string>
 #include <vector>
 
 using namespace std;
-using Array = vector<int>;
-using Arrays = vector<string>;
+using Arrays = vector<char>;
+using Array = vector<string>;
 
 string FindBase(int Base, int Number);
 
 int main(){
+	Array output;
 
-    Array Input;
-    Arrays Output;
+	int UserInput;
+	int BaseInput;
+	
+	cin>>BaseInput;
+	cin>>UserInput;
 
-    int BaseInput = -1;
-    int UserInput = -1;
+	while(UserInput != -1){
+		output.push_back(FindBase(BaseInput,UserInput));
+		cin>>UserInput;
+	}
 
-    cin >> BaseInput;
-    cin >> UserInput;
+	for(string i:output){
+		cout<<i<<"\n";
+	
+	}
 
-    while (UserInput != -1){
-        Input.push_back(UserInput);
-        cin >> UserInput;
-    }
-    
-    for(int index : Input){
-        Output.push_back(FindBase(BaseInput, index)); // Changed UserInput to index
-    }
-
-    for(string output : Output){
-        cout << output << "\n";
-    }
-    
-    return EXIT_SUCCESS;
+	return 0;
 }
 
 
-string FindBase(int Base, int Number){
+string FindBase(int Base,int Number){
 
-    Array Bases = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	Arrays Bases = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
          'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
          'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-    int Zero = Number;
-    int index = -1;
-    string Output = "";
 
-    do
-    {
-        Zero = Zero/Base;
-        index = Zero%Base;
-        Output += Bases[index];
-    } while (Zero != 0);
-
-    return Output;
+	int Num(Number),modulo;
+	string output;
+	
+	while(Num != 0){
+		modulo = Num%Base;
+		Num = Num/Base;
+		output += Bases[modulo];
+	}
+	reverse(output.begin(), output.end());
+	return output;
 }
